82661c4c5de14724cc15e83e265b23314fdfdf2c
Wassim CHEGHAM <github@wassimchegham.com>
2018-05-24 01:32:20 +0200

    chore: handle blob images + refactoring

--- projects/manekinekko/ngx-sketch-viewer/src/lib/sketch.service.ts
+++ projects/manekinekko/ngx-sketch-viewer/src/lib/sketch.service.ts
@@ -1,56 +1,123 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
-export interface SketchContent {
-  pages: any[];
-  previews: any[];
+export interface SketchUser {
+  [key: string]: {
+    // "{-1185.8387361675846, 178}"
+    scrollOrigin: string;
+    zoomValue: number;
+  };
+}
+
+export interface SketchData {
+  pages: Array<SketchMSPage>;
+  previews: Array<{ source: string; width: number; height: number }>;
+  document: SketchMSDocumentData;
+  user: {};
+  meta: SketchMSMetadata;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class SketchService {
-  constructor() {}
+  _data: SketchData;
+  constructor(private sanitizer: DomSanitizer) {
+    this._data = {
+      pages: [],
+      previews: [],
+      document: {},
+      user: {},
+      meta: {}
+    } as any;
+  }
 
   async process(file: File) {
-    const json = await this.parse(file);
-    console.log(json);
+    this._data = await this.parse(file);
+    console.log(this._data);
 
-    return json;
+    return this._data;
   }
 
   async parse(file) {
-    const output: SketchContent = {
-      pages: [],
-      previews: []
-    };
-    const reader = new FileReader();
-    reader.onload = async e => {
-      const data = (e.target as FileReader).result;
+    return new Promise<SketchData>((resolve, reject) => {
+      const _data: SketchData = {
+        pages: [],
+        previews: [],
+        document: {},
+        user: {},
+        meta: {}
+      } as any;
+      const reader = new FileReader();
+      reader.onload = async readerEvent => {
+        const data = (readerEvent.target as FileReader).result;
 
-      const zip = await window['JSZip'].loadAsync(data);
-      zip.forEach(async (relativePath, zipEntry) => {
-        if (relativePath === 'previews/preview.png') {
-          const content = await zipEntry.async('base64');
+        const zip = await window['JSZip'].loadAsync(data);
 
-          try {
-            output.previews.push(`data:image/png;base64,${content}`);
-          } catch (e) {
-            throw new Error('Could not load a Sketch preview');
+        zip.forEach(async (relativePath, zipEntry) => {
+          if (relativePath === 'previews/preview.png') {
+            const content = await zipEntry.async('base64');
+            const source = `data:image/png;base64,${content}`;
+
+            // compute the image preview size
+            const image = new Image();
+            image.onload = imageLoadEvent => {
+              _data.previews.push({
+                source,
+                width: image.width,
+                height: image.height
+              });
+            };
+            image.onerror = error => {
+              reject('Could not load a Sketch preview');
+            };
+            image.src = source;
+          } else if (relativePath.startsWith('pages/')) {
+            const content = await zipEntry.async('string');
+
+            try {
+              const page = JSON.parse(content) as SketchMSPage;
+              _data.pages.push(page);
+            } catch (e) {
+              reject('Could not load page');
+            }
+          } else if (relativePath.startsWith('images/')) {
+            const blob = await zipEntry.async('blob');
+            const objectUrl = URL.createObjectURL(blob);
+
+            // compute the image preview size
+            const image = new Image();
+            image.onload = imageLoadEvent => {
+              _data.previews.push({
+                source: objectUrl,
+                width: image.width,
+                height: image.height
+              });
+            };
+            image.onerror = error => {
+              reject('Could not load a Sketch preview');
+            };
+            
+            // @todo deal with SafeResourceUrl!!
+            image.src = this.sanitizer.bypassSecurityTrustResourceUrl(objectUrl).toString();
+
+          } else {
+            // document.json
+            // user.json
+            // meta.json
+            const content = await zipEntry.async('string');
+            const json = JSON.parse(content);
+            _data[relativePath.replace('.json', '')] = json;
           }
-        } else if (relativePath.startsWith('pages/')) {
-          const content = await zipEntry.async('string');
+        });
 
-          try {
-            const page = JSON.parse(content);
-            output.pages.push(page);
-          } catch (e) {
-            throw new Error('Could not load page');
-          }
-        }
-      });
-    };
-    reader.readAsArrayBuffer(file);
+        resolve(_data);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }
 
-    return output;
+  getPages(): SketchMSPage[] {
+    return this._data.pages;
   }
 }
