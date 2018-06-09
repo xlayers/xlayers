cad641a783f9488897b1754c4a2729813f7b2740
Wassim CHEGHAM <github@wassimchegham.com>
2018-06-10 01:19:30 +0200

    chore: use SettingsLayerPositionComponent

--- src/app/layer-settings/layer-settings.module.ts
+++ src/app/layer-settings/layer-settings.module.ts
@@ -2,13 +2,18 @@ import { CoreModule } from './../core/core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsContainerComponent } from './settings-container/settings-container.component';
-import { SettingsLayerComponent } from './settings-layer/settings-layer.component';
+import { SettingsLayerPositionComponent } from './settings-layer-position/settings-layer-position.component';
 import { SettingsLayerPropertiesComponent } from './settings-layer-properties/settings-layer-properties.component';
 import { SettingsLayerColorsComponent } from './settings-layer-colors/settings-layer-colors.component';
 
 @NgModule({
   imports: [CommonModule, CoreModule],
-  declarations: [SettingsContainerComponent, SettingsLayerComponent, SettingsLayerPropertiesComponent, SettingsLayerColorsComponent],
+  declarations: [
+    SettingsContainerComponent,
+    SettingsLayerPositionComponent,
+    SettingsLayerPropertiesComponent,
+    SettingsLayerColorsComponent
+  ],
   exports: [SettingsContainerComponent]
 })
 export class LayerSettingsModule {}
