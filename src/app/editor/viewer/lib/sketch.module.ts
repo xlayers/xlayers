7211ecb3ab3364660213fa62499181f3f7bcf563
Wassim CHEGHAM <github@wassimchegham.com>
2018-06-12 02:00:26 +0200

    chore: import SketchTooltilDirective

--- src/app/editor/viewer/lib/sketch.module.ts
+++ src/app/editor/viewer/lib/sketch.module.ts
@@ -7,6 +7,7 @@ import { SketchDropzoneComponent } from './sketch-dropzone.component';
 import { SketchLayerComponent } from './sketch-layer.component';
 import { SketchPageComponent } from './sketch-page.component';
 import { SketchStopEventPropagationDirective } from './sketch-stop-event-propagation.directive';
+import { SketchTooltilDirective } from './sketch-tooltip.directive';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { SketchStopEventPropagationDirective } from './sketch-stop-event-propaga
     SketchPageComponent,
     SketchLayerComponent,
     SketchStopEventPropagationDirective,
-    SketchSelectedLayerDirective
+    SketchSelectedLayerDirective,
+    SketchTooltilDirective
   ],
   exports: [SketchCanvasComponent, SketchDropzoneComponent, SketchContainerComponent, SketchSelectedLayerDirective]
 })
