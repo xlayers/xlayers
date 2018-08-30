0d6cd9c34bcc8fafdd710ef713a9ff78b313fd7c
Wassim Chegham <github@wassimchegham.com>
2018-08-30 17:44:45 +0200

    chore: add build number in landing page

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'sketch-landing',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+
+  version = environment.version;
 
   constructor() { }
 
