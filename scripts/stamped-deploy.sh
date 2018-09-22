e6bdc1b5cc89ef463976744b982359933e69a77e
Wassim Chegham <github@wassim.app>
2018-09-22 19:28:55 +0200

    build: rename script to stamp-deploy.sh

---
+++ scripts/stamped-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z "${SHORT_SHA}" ]]; then
+    b=`git rev-parse --abbrev-ref HEAD`
+    v=`git rev-parse --short HEAD`
+    version="$b+sha.$v"
+else
+    version="${DEPLOY_ENV}"
+fi
+
+## replease _BUILD_HASH_ with the current build number
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js > /dev/null
+
+status=$?
+if [ $status -eq 0 ];then
+   echo "Deploy was stamped: $version"
+else
+   echo "Could not stamp this eploy!"
+fi
