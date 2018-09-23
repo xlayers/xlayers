b51caedb085c2d487fafabbdb1967658d6de65b2
Wassim Chegham <github@wassim.app>
2018-09-23 10:22:40 +0000

    fix: the stamp script

--- scripts/stamped-deploy.sh
+++ scripts/stamped-deploy.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ -z "${SHORT_SHA}" ]]; then
+set -u
+
+SHORT_SHA=${SHORT_SHA-default}
+BRANCH_NAME=${BRANCH_NAME-default}
+
+if [[ -z "$SHORT_SHA" ]]; then
     b=`git rev-parse --abbrev-ref HEAD`
     v=`git rev-parse --short HEAD`
     version="$b+sha.$v"
 else
+    echo "Cloud Build environment detected"
     version="${BRANCH_NAME}+sha.${SHORT_SHA} (preview)"
 fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js > /dev/null
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js
 
 status=$?
 if [ $status -eq 0 ];then
