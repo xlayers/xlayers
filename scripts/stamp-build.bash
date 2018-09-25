ab3ccbdcaf9ea192cdfdc9a7484b283a616f9aea
Wassim Chegham <github@wassim.app>
2018-09-25 12:55:47 +0200

    build: change stamp version pattern

--- scripts/stamp-build.bash
+++ scripts/stamp-build.bash
@@ -1,15 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [[ -z "${SHORT_SHA}" ]]; then
     b=`git rev-parse --abbrev-ref HEAD`
     v=`git rev-parse --short HEAD`
-    version="$b+sha.$v"
+    version="$b#$v"
 else
     version="${DEPLOY_ENV}"
 fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js > /dev/null
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js
 
 status=$?
 if [ $status -eq 0 ];then
