99101a37e872359e493d5c069a14c88f840d7e16
Wassim Chegham <github@wassim.app>
2018-09-27 15:55:13 +0200

    build: rename the build stamp file

--- scripts/stamp-build.bash
+++ scripts/stamp-build.bash
@@ -1,19 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [[ -z "${SHORT_SHA}" ]]; then
-    b=`git rev-parse --abbrev-ref HEAD`
-    v=`git rev-parse --short HEAD`
-    version="$b#$v"
+if [[ -n "$BUILD_ID" ]]; then
+    echo "Cloud Build environment detected!"
+    version="${BUILD_ID}"
 else
-    version="${DEPLOY_ENV}"
+    b=`git rev-parse --abbrev-ref HEAD || echo "n-a"`
+    v=`git rev-parse --short HEAD || echo "0000000"`
+    version="$b#$v (`date`)"
 fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/html/main.*.js
+
 
 status=$?
 if [ $status -eq 0 ];then
-   echo "Deploy was tagged: $version"
+    echo "$version" > dist/html/version.txt
+    echo "Build was stamped: `cat dist/html/version.txt`"
 else
-   echo "Could not tag this eploy!"
+   echo "Could not stamp this build!"
 fi
