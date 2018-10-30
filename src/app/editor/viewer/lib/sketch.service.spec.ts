a71298cc54e78926948117abb3dca9590a403662
Wassim Chegham <github@wassim.app>
2018-10-30 18:30:08 +0100

    refactor: remove the SketchStyleParserService from the unit test

--- src/app/editor/viewer/lib/sketch.service.spec.ts
+++ src/app/editor/viewer/lib/sketch.service.spec.ts
@@ -2,34 +2,38 @@ import { SketchService, SketchData } from './sketch.service';
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { getFileMock } from './sketch-container.component.mock';
-import { SketchStyleParserService } from './parsers/sketch-style-parser.service';
 import { CommonModule } from '@angular/common';
 import { getSketchDataMock } from './sketch.service.mock';
 
+const sketchStyleParserService = {
+  visit() {}
+};
+
 describe('SketchService', () => {
   let sketchService: SketchService;
-  let sketchColorParser: SketchStyleParserService;
   let mockFile: File;
   let mockSketchData: SketchData;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, CommonModule],
-      providers: [SketchService, SketchStyleParserService]
+      providers: [
+        SketchService
+      ]
     });
   }));
 
   beforeEach(() => {
     sketchService = TestBed.get(SketchService);
-    sketchColorParser = TestBed.get(SketchStyleParserService);
     mockFile = getFileMock();
     mockSketchData = getSketchDataMock();
   });
 
   it('should process file', (done: DoneFn) => {
-    spyOn(sketchService, 'sktech2Json').and.returnValue(Promise.resolve(mockSketchData));
-    spyOn(sketchColorParser, 'visit').and.returnValue(null);
-    sketchService.process(mockFile).then((element) => {
+    spyOn(sketchService, 'sketch2Json').and.returnValue(
+      Promise.resolve(mockSketchData)
+    );
+    sketchService.process(mockFile).then(element => {
       expect(element).toBe(mockSketchData);
       done();
     });
