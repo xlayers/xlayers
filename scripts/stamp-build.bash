2271248e6840f3190dacacfc64dc6a8431ab3eae
Wassim Chegham <github@wassim.app>
2018-10-30 17:39:25 +0100

    feat: Google Cloud Build support (#57)
    * build: use cluster-1 when copying to nginx
    
    * build: tag artifacts on Cloud Build
    
    * build: switch to *.angular.run for previewing
    
    * build: use Cloud Build vars when stamping
    
    * build: use a generic YAML config
    
    * buid: swith to bash to run the stamp script
    
    * fix: the stamp script
    
    * build: debug the build env vars
    
    * build: clean the couldbuild config
    
    * refactor: rename the build stamp script and use the BUILD_ID
    
    * build: add env vars when apply Ingress config
    
    * build: use a templated YAML config for deployments
    
    * refactor: remove the $ sign from the YAML templating config
    
    * buil: enable the build steps again
    
    * buil: rename subdomaine to xlayers-SHA
    
    * docs: update app title
    
    * docs: update twitter description

--- scripts/stamp-build.bash
+++ scripts/stamp-build.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+set -u
+
 if [[ -n "$BUILD_ID" ]]; then
     echo "Cloud Build environment detected!"
     version="${BUILD_ID}"
 else
-    b=`git rev-parse --abbrev-ref HEAD || echo "n-a"`
-    v=`git rev-parse --short HEAD || echo "0000000"`
-    version="$b#$v (`date`)"
+    b=`git rev-parse --abbrev-ref HEAD`
+    v=`git rev-parse --short HEAD`
+    version="$b+sha.$v"
 fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/html/main.*.js
-
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/*/main.*.js
 
 status=$?
 if [ $status -eq 0 ];then
-    echo "$version" > dist/html/version.txt
-    echo "Build was stamped: `cat dist/html/version.txt`"
+   echo "Build was stamped: $version"
 else
    echo "Could not stamp this build!"
 fi
