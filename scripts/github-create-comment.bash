3145ce507d9900c5252839c94e976d1c155edc60
Wassim Chegham <github@wassim.app>
2018-11-01 03:15:05 +0100

    chore: add verbosity when running curl commands

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -16,13 +16,13 @@ query='{
 body='Preview: $PREVIEW_BUILD_URL'
 
 echo ">> Getting the subject ID from GitHub (access_token=$access_token)"
-subject_id=$(curl -s -X POST \
+subject_id=$(curl -v -X POST \
     --url 'https://api.github.com/graphql?access_token=${access_token}' \
     --header 'content-type: application/json' \
     --data '{ "query": "${query}" }' | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
 
 echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
-curl -X POST \
+curl -v -X POST \
     --url 'https://api.github.com/graphql?access_token=$access_token' \
     --header 'content-type: application/json' \
     --data '{ "query": "mutation AddCommentToIssue { addComment(input: { subjectId: \"$subject_id\",body: \"$body\"}) { clientMutationId }}" }'
