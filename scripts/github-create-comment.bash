8fd2ec447e1f9fe9358bb2c4e0c7d83417ec1d84
Wassim Chegham <github@wassim.app>
2018-11-01 03:59:15 +0100

    chore: fix second access_token

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -4,7 +4,7 @@ set -u
 
 access_token=$GITHUB_ACCESS_TOKEN
 
-echo ">> Getting the subject ID from GitHub (access_token=$(access_token))"
+echo ">> Getting the subject ID from GitHub (access_token=$access_token)"
 search_query='{
     search(first: 1, type: ISSUE, query: "type:pr repo:xlayers/xlayers $COMMIT_SHA") {
         nodes {
@@ -17,10 +17,10 @@ search_query='{
 subject_id=`curl -v \
     --url "https://api.github.com/graphql?access_token=$access_token" \
     --header 'content-type: application/json' \
-    --data '{ "query": "$(search_query)" }' | \
+    --data '{ "query": "$search_query" }' | \
     python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]'`
 
-echo ">> Sending the Preview Link on issue $subject_id (access_token=$(access_token))"
+echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
 mutate_query='{
     mutation AddCommentToIssue {
         addComment(input: {subjectId: "$subject_id", body: "$body"}) {
@@ -30,6 +30,6 @@ mutate_query='{
 }'
 body='Preview: $PREVIEW_BUILD_URL'
 curl -v \
-    --url 'https://api.github.com/graphql?access_token=$access_token' \
+    --url "https://api.github.com/graphql?access_token=$access_token" \
     --header 'content-type: application/json' \
     --data '{ "query": "$(mutate_query)" }'
