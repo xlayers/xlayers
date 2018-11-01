761d1c4e22b0c7fc168bd9d0fddaecf5a05f0157
Wassim Chegham <github@wassim.app>
2018-11-01 03:46:10 +0100

    chore: clean up the shell script

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -4,7 +4,8 @@ set -u
 
 access_token=$GITHUB_ACCESS_TOKEN
 
-query='{
+echo ">> Getting the subject ID from GitHub (access_token=$(access_token))"
+search_query='{
     search(first: 1, type: ISSUE, query: "type:pr repo:xlayers/xlayers $COMMIT_SHA") {
         nodes {
         ... on PullRequest {
@@ -13,16 +14,22 @@ query='{
         }
     }
 }'
-body='Preview: $PREVIEW_BUILD_URL'
-
-echo ">> Getting the subject ID from GitHub (access_token=$access_token)"
 subject_id=$(curl -v -X POST \
-    --url "https://api.github.com/graphql?access_token=$access_token" \
+    --url "https://api.github.com/graphql?access_token=$(access_token)" \
     --header 'content-type: application/json' \
-    --data "{ 'query': '$query' }" | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
+    --data '{ "query": "$(search_query)" }' | \
+    python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
 
-echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
+echo ">> Sending the Preview Link on issue $subject_id (access_token=$(access_token))"
+mutate_query='{
+    mutation AddCommentToIssue {
+        addComment(input: {subjectId: "$subject_id", body: "$body"}) {
+            clientMutationId
+        }
+    }
+}'
+body='Preview: $PREVIEW_BUILD_URL'
 curl -v -X POST \
-    --url 'https://api.github.com/graphql?access_token=$access_token' \
+    --url 'https://api.github.com/graphql?access_token=$(access_token)' \
     --header 'content-type: application/json' \
-    --data '{ "query": "mutation AddCommentToIssue { addComment(input: { subjectId: \"$subject_id\",body: \"$body\"}) { clientMutationId }}" }'
+    --data '{ "query": "$(mutate_query)" }'
