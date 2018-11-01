3c88dda6abcac060e14779ff85ca8b8f647701b6
Wassim Chegham <github@wassim.app>
2018-11-01 02:52:22 +0100

    chore: add more logs in shell script

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -12,11 +12,14 @@ query='{
     }
 }'
 body='Preview: $PREVIEW_BUILD_URL'
+
+echo ">> Getting the subject ID from GitHub (access_token=$ACCESS_TOKEN)"
 subjectId=$(curl -s -X POST \
     --url 'https://api.github.com/graphql?access_token=$ACCESS_TOKEN' \
     --header 'content-type: application/json' \
     --data '{ "query": "$query" }' | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
 
+echo ">> Sending the Preview Link on issue $subjectId (access_token=$ACCESS_TOKEN)"
 curl -X POST \
     --url 'https://api.github.com/graphql?access_token=$ACCESS_TOKEN' \
     --header 'content-type: application/json' \
