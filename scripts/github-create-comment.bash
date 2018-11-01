7b3c1abdfd2d338a1b5e61151fbf782c926fb39c
Wassim Chegham <github@wassim.app>
2018-11-01 04:08:23 +0100

    chore: fix quotes

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -21,14 +21,14 @@ subject_id=`curl \
     python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]'`
 
 echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
-mutation_query='{
+body='Preview: $PREVIEW_BUILD_URL'
+mutation_query="{
     mutation AddCommentToIssue {
-        addComment(input: {subjectId: "$subject_id", body: "$body"}) {
+        addComment(input: {subjectId: \"$subject_id\", body: \"$body\"}) {
             clientMutationId
         }
     }
-}'
-body='Preview: $PREVIEW_BUILD_URL'
+}"
 curl \
     --url "https://api.github.com/graphql?access_token=$access_token" \
     --header 'content-type: application/json' \
