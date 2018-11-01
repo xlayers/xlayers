d24975344adfe44711ea464b434ff816fbcbcd49
Wassim Chegham <github@wassim.app>
2018-11-01 03:20:22 +0100

    chore: use double quotes to evaluate variable

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -17,9 +17,9 @@ body='Preview: $PREVIEW_BUILD_URL'
 
 echo ">> Getting the subject ID from GitHub (access_token=$access_token)"
 subject_id=$(curl -v -X POST \
-    --url 'https://api.github.com/graphql?access_token=${access_token}' \
+    --url "https://api.github.com/graphql?access_token=${access_token}" \
     --header 'content-type: application/json' \
-    --data '{ "query": "${query}" }' | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
+    --data "{ 'query': '${query' }" | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]')
 
 echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
 curl -v -X POST \
