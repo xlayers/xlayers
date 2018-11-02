663979c23728b0e633382398d645a38feb98e7a7
Wassim Chegham <github@wassim.app>
2018-11-02 01:56:52 +0100

    chore: remove curl logs

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -6,7 +6,7 @@ access_token=$GITHUB_ACCESS_TOKEN
 
 echo ">> Getting the subject ID for commit=$COMMIT_SHA (access_token=$access_token)"
 
-subject_id=$(curl  -v  --url "https://api.github.com/graphql?access_token=$access_token" \
+subject_id=$(curl --url "https://api.github.com/graphql?access_token=$access_token" \
         --header 'content-type: application/json' \
         --data "{ \"query\": \"{ search(first: 1, type: ISSUE, query: \\\"type:pr repo:xlayers/xlayers $(COMMIT_SHA)\\\") { nodes { ... on PullRequest { id, number, title } } } }\" }")
 
