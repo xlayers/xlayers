7efe332bf4171d46729c0364306cb13a20d1d27b
Wassim Chegham <github@wassim.app>
2018-11-02 02:18:37 +0100

    chore: add comment + update github comment

--- scripts/github-create-comment.bash
+++ scripts/github-create-comment.bash
@@ -6,14 +6,15 @@ access_token=$GITHUB_ACCESS_TOKEN
 
 echo ">> Getting the subject ID for commit=$COMMIT_SHA (access_token=$access_token)"
 
-subject_id=$(curl --url "https://api.github.com/graphql?access_token=$access_token" \
+subject_json=$(curl --url "https://api.github.com/graphql?access_token=$access_token" \
         --header 'content-type: application/json' \
         --data "{ \"query\": \"{ search(first: 1, type: ISSUE, query: \\\"type:pr repo:xlayers/xlayers $COMMIT_SHA\\\") { nodes { ... on PullRequest { id, number, title } } } }\" }")
 
 echo ">> Received..."
-echo $subject_id
+echo $subject_json
 
-subject_id=`echo $subject_id | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]'`
+# extract the subject ID from the JSON string using Python's JSON module
+subject_id=`echo $subject_json | python -c 'import sys, json; print json.load(sys.stdin)["data"]["search"]["nodes"][0]["id"]'`
 
 if [ $? -eq 1 ]; then
     echo "Could not get Subject ID. Abort."
@@ -22,7 +23,7 @@ if [ $? -eq 1 ]; then
 fi
 
 echo ">> Sending the Preview Link on issue $subject_id (access_token=$access_token)"
-body="Preview: $PREVIEW_BUILD_URL"
+body="☸️ Build auto-deployed at: $PREVIEW_BUILD_URL"
 
 curl --url "https://api.github.com/graphql?access_token=$access_token" \
   --header 'content-type: application/json' \
