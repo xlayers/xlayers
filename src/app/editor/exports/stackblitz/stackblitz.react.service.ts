86c304c66b394d40b72b66e603e373a572b6b0e1
Wassim Chegham <github@wassim.app>
2018-12-21 18:18:15 +0100

    fix: fix react's folder layout for stackblitz

--- src/app/editor/exports/stackblitz/stackblitz.react.service.ts
+++ src/app/editor/exports/stackblitz/stackblitz.react.service.ts
@@ -38,10 +38,25 @@ export class ExportStackblitzReactService {
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import XLayers from './src/XLayers';
+import XLayers from './XLayers';
 
 ReactDOM.render(<XLayers />, document.getElementById('root'));
 `;
+    files['src/index.css'] = `
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}`;
 
     return {
       files,
