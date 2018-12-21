77ccd5ef8d9361b9a257aa361363345c7dbc69c6
Wassim Chegham <github@wassim.app>
2018-12-22 00:16:50 +0100

    refactor: use cdkDrag + remove resizing feature
    fixes #100

--- src/app/editor/viewer/lib/sketch-layer.component.mock.ts
+++ src/app/editor/viewer/lib/sketch-layer.component.mock.ts
@@ -1,34 +1,7 @@
-import { ResizeEvent } from 'angular-resizable-element';
-
-export function getResizeEventMock({
-  positive = true,
-  top = true,
-  bottom = true,
-  left = true,
-  right = true
-}: {
-  positive?: boolean;
-  top?: boolean;
-  bottom?: boolean;
-  left?: boolean;
-  right?: boolean;
-} = {}) {
-  return {
-    rectangle: {
-      width: right && 38,
-      height: bottom && 98
-    },
-    edges: {
-      top:  top && (positive ? 917 : -67),
-      left: left && (positive ? 578 : -346)
-    }
-  } as ResizeEvent;
-}
-
 export function getFrameMock(x: number, y: number) {
   return {
-      x,
-      y
+    x,
+    y
   } as SketchMSRect;
 }
 
@@ -36,7 +9,7 @@ export function getFlatLayerMock(layer_number: number = 1) {
   return {
     do_objectID: `page-layer`,
     _class: 'page',
-    layers: Array.from(Array(layer_number).keys()).map((index) => ({
+    layers: Array.from(Array(layer_number).keys()).map(index => ({
       do_objectID: `layer-${index}-id`,
       _class: 'layer',
       layers: [],
