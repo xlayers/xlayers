14557c81dada210326f7efef70ede58225f9cb81
Wassim Chegham <github@wassim.app>
2019-01-09 23:40:04 +0100

    fix: use the correct WC filename

--- src/app/editor/code-editor/editor-container/codegen/wc/wc.service.ts
+++ src/app/editor/code-editor/editor-container/codegen/wc/wc.service.ts
@@ -3,26 +3,27 @@ import { CodeGenFacade, XlayersNgxEditorModel } from '../codegen.service';
 import { SharedCodegen } from '../shared-codegen.service';
 import { wcTemplate, readmeTemplate } from './wc.template';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class WCCodeGenService implements CodeGenFacade {
-
   constructor(private sharedCodegen: SharedCodegen) {}
 
   generate(ast: SketchMSLayer): Array<XlayersNgxEditorModel> {
-    return [{
-      uri: 'README.md',
-      value: this.generateReadme(),
-      language: 'markdown',
-      kind: 'text'
-    }, {
-      uri: 'xlayers.js',
-      value: this.generateComponent(ast),
-      language: 'javascript',
-      kind: 'wc'
-    }];
+    return [
+      {
+        uri: 'README.md',
+        value: this.generateReadme(),
+        language: 'markdown',
+        kind: 'text'
+      },
+      {
+        uri: 'x-layers-element-file.js',
+        value: this.generateComponent(ast),
+        language: 'javascript',
+        kind: 'wc'
+      }
+    ];
   }
 
   private generateReadme() {
@@ -30,6 +31,9 @@ export class WCCodeGenService implements CodeGenFacade {
   }
 
   private generateComponent(ast: SketchMSLayer) {
-    return wcTemplate(this.sharedCodegen.generateComponentTemplate(ast , 1) , this.sharedCodegen.generateComponentStyles(ast));
+    return wcTemplate(
+      this.sharedCodegen.generateComponentTemplate(ast, 1),
+      this.sharedCodegen.generateComponentStyles(ast)
+    );
   }
 }
