4e0470b8a946bfa5f1b4a6ee8e1b52516271f4ed
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2019-02-05 11:43:04 +0100

    feat: New Custom Errorhandler (#168)
    Changed errorhandler to use the store to inform the user.

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { UiState } from 'src/app/core/state/ui.state';
 import { CodeEditorModule } from 'src/app/editor/code-editor/code-editor.module';
+import { CodeGenState } from '../core/state/page.state';
 import { CoreModule } from './../core/core.module';
 import { EditorComponent } from './editor.component';
+import { ErrorReportService } from './error-report.service';
 import { LayerSettingsModule } from './layer-settings/layer-settings.module';
 import { TreeViewComponent } from './tree-view/tree-view.component';
 import { AngularSketchModule } from './viewer/lib/sketch.module';
-import { CodeGenState } from '../core/state/page.state';
 
 export const routes: Route[] = [
   {
@@ -30,6 +31,12 @@ export const routes: Route[] = [
     LayerSettingsModule,
     CodeEditorModule
   ],
-  declarations: [EditorComponent, TreeViewComponent]
+  declarations: [EditorComponent, TreeViewComponent],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: ErrorReportService
+    }
+  ]
 })
 export class EditorModule {}
