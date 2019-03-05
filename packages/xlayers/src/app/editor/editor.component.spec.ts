ab2814025df9055346cbad26081a636b93f05cf2
Wassim Chegham <github@wassim.app>
2019-03-05 16:24:15 +0100

    feat: add badge for the current environnement

--- packages/xlayers/src/app/editor/editor.component.spec.ts
+++ packages/xlayers/src/app/editor/editor.component.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
-import { EditorComponent } from './editor.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
-import { NgxsModule } from '@ngxs/store';
 import { XStore } from '@app/core/state/state.mock';
+import { WINDOW_PROVIDERS } from '@app/core/window.service';
+import { NgxsModule } from '@ngxs/store';
+import { EditorComponent } from './editor.component';
 
 describe('EditorComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [MatMenuModule, NgxsModule.forRoot([XStore])],
-      declarations: [
-        EditorComponent
-      ],
+      declarations: [EditorComponent],
+      providers: [WINDOW_PROVIDERS]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
