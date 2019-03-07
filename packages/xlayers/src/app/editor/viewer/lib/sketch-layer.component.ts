f46e8c4e812c0eaa666baa6ba687b70692d13181
Shikanime Deva <22115108+Shikanime@users.noreply.github.com>
2019-03-07 07:32:18 -0800

    fix: preview border (#250)
    * fix: preview border
    
    * chore: replace outline with box-shadow

--- packages/xlayers/src/app/editor/viewer/lib/sketch-layer.component.ts
+++ packages/xlayers/src/app/editor/viewer/lib/sketch-layer.component.ts
@@ -39,24 +39,23 @@ import { CurrentLayer, UiState } from '@app/core/state/ui.state';
   styles: [
     `
       :host {
-        border: 1px solid transparent;
-        box-sizing: border-box;
-        transition: border-color 0.1s linear, transform 1s;
+        box-shadow: 0 0 0 1px transparent;
+        transition: box-shadow 0.1s linear, transform 1s;
         transform-origin: 0 0;
         transform-style: preserve-3d;
         will-change: transform, transition;
       }
 
       :host(:hover) {
-        border-color: #51c1f8 !important;
+        box-shadow: 0 0 0 1px #51c1f8 !important;
         background-color: rgba(81, 193, 248, 0.2);
       }
       :host(.isCurrentLayer) {
-        border-color: #ee4743 !important;
+        box-shadow: 0 0 0 1px #ee4743 !important;
         background-color: rgba(238, 71, 67, 0.2);
       }
       :host(.wireframe) {
-        border-color: black;
+        box-shadow: 0 0 0 1px black;
       }
     `
   ]
