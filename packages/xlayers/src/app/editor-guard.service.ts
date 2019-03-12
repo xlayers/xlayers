d9d54371619966a0a94a6fc45e3022f5f7662132
Jefiozie <jefiozie.bosch@gmail.com>
2019-03-12 19:05:18 +0100

    fix: guard will redirect when no page selected

--- packages/xlayers/src/app/editor-guard.service.ts
+++ packages/xlayers/src/app/editor-guard.service.ts
@@ -1,54 +1,39 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from '@angular/router';
 import { UiState } from '@app/core/state';
+import { Navigate } from '@ngxs/router-plugin';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { filter, map, take, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorGuardService implements CanActivateChild, CanActivate {
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store) { }
 
   isValidSession() {
-    return this.store.select(UiState.currentPage).pipe(
-      filter(page => !!page),
-      take(1)
-    );
+    return this.store.selectOnce(UiState.currentPage).pipe(map(page => {
+      if (page) {
+        return true;
+      } else {
+        this.store.dispatch(new Navigate(['/upload']));
+        return false;
+      }
+    }));
   }
 
   canActivate(
     _route: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot
   ): Observable<boolean> {
-    return this.isValidSession().pipe(
-      tap(x => console.log('canActivate', x)),
-      map(page => {
-        if (page) {
-          return true;
-        } else {
-          // this.store.dispatch(new Navigate(['/upload']));
-          return false;
-        }
-      })
-    );
+    return this.isValidSession();
   }
 
   canActivateChild(
     _route: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot
   ): Observable<boolean> {
-    return this.isValidSession().pipe(
-      tap(x => console.log('canActivateChild', x)),
-      map(page => {
-        if (page) {
-          return true;
-        } else {
-          // this.store.dispatch(new Navigate(['/upload']));
-          return false;
-        }
-      })
-    );
+    return this.isValidSession();
   }
 }
