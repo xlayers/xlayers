f0a5168ec4a3f13e07cae4fed713339ed8638e4d
Shikanime Deva <deva.shikanime@protonmail.com>
2019-08-21 09:13:12 +0200

    refactor: remove useless codecomponent oninit

--- src/app/editor/code/code.component.ts
+++ src/app/editor/code/code.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'xly-code',
   templateUrl: './code.component.html',
   styleUrls: ['./code.component.css']
 })
-export class CodeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CodeComponent {}
