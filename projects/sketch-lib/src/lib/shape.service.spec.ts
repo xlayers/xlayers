0b4ade4164190fd68e83e65f096af579e3edfd68
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2019-11-16 18:59:26 +0100

    test(sketch-lib): add initial tests

---
+++ projects/sketch-lib/src/lib/shape.service.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { ShapeService } from './shape.service';
+
+describe.only('ShapeService', () => {
+    let service: ShapeService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ShapeService]
+        });
+
+        service = TestBed.get(ShapeService);
+    });
+
+    it('should create', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should handle points to x y numbers', () => {
+        const data = {
+            frame: {
+                width: '0',
+                height: '0'
+            }
+        } as any;
+
+        const actual = service.parsePoint('0.0043562068495639136, 0.52083333333333326', 1, data);
+        expect(actual).toEqual({ 'x': 1, 'y': 1 });
+    });
+});
