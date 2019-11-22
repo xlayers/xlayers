706c858b7afbd9b728fc8a0a914dd5eae01fdc20
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2019-11-22 13:26:36 +0100

    test: added test for stencil-codegen

---
+++ projects/stencil-codegen/src/lib/stencil-aggregator.service.spec.ts
@@ -0,0 +1,67 @@
+import { TestBed } from '@angular/core/testing';
+import { StencilAggregatorService } from './stencil-aggregator.service';
+
+describe('StencilAggregatorService', () => {
+    let service: StencilAggregatorService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [StencilAggregatorService]
+        });
+
+        service = TestBed.get(StencilAggregatorService);
+    });
+
+    it('should create', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should have three files for a basic react component', () => {
+        const data = {
+            '_class': 'rectangle',
+            'name': 'abc',
+            'web': {
+            },
+
+            layers: [
+                {
+                    '_class': 'text',
+                    'css': {
+                        'className': 'aclass',
+                        rules: {
+                            'width': '20px'
+                        }
+                    },
+                    'web': {
+
+                    },
+                    attributedString: { string: 'attr' }
+                }
+            ]
+        } as any;
+        const aggregated = service.aggregate(data, {
+            textTagName: 'span',
+            bitmapTagName: 'img',
+            blockTagName: 'div',
+            xmlPrefix: 'xly-',
+            cssPrefix: 'xly_',
+            componentDir: 'components',
+            assetDir: 'assets',
+        });
+
+        expect(aggregated.length).toEqual(3);
+
+        const [component, test, css] = aggregated;
+        expect(component.kind).toEqual('stencil');
+        expect(component.language).toEqual('typescript');
+        expect(component.value.indexOf('<span >attr</span>') >= 0).toBeTruthy();
+        expect(component.uri).toEqual('components/abc.tsx');
+        expect(test.kind).toEqual('stencil');
+        expect(test.uri).toEqual('components/abc.e2e.ts');
+        expect(test.language).toEqual('typescript');
+        expect(css.kind).toEqual('stencil');
+        expect(css.language).toEqual('css');
+        expect(css.uri).toEqual('components/abc.css');
+        expect(css.value.indexOf(`.aclass`) >= 0).toEqual(true);
+    });
+});
---
+++ projects/stencil-codegen/src/lib/stencil-docgen.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { StencilDocGenService } from './stencil-docgen.service';
+
+describe('StencilDocGenService', () => {
+    let service: StencilDocGenService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [StencilDocGenService]
+        });
+
+        service = TestBed.get(StencilDocGenService);
+    });
+
+    it('should create', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should have all the correct propertys', () => {
+        const data = {
+            meta: { app: 'THE_APP' }
+        } as any;
+        const actual = service.aggregate(data);
+
+        expect(actual[0].kind).toEqual('text');
+        expect(actual[0].language).toEqual('markdown');
+        expect(actual[0].uri).toEqual('README.md');
+        // WE GET THE FIRST LINE OF THE README
+        expect(actual[0].value.split('\n')[0]).toEqual('## How to use the THE_APP StencilJS Web Components');
+    });
+});
