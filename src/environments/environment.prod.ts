05a8ddfa7366ddf22e6064fe67c4861ff28c2879
Wassim Chegham (on paternity leave) <1699357+manekinekko@users.noreply.github.com>
2019-12-12 17:08:29 +0100

    chore: update demo files to Sketch v59 (#342)
    closes #340

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,38 +5,6 @@ export const environment = {
   baseUrl: 'https://xlayers.app',
   demoFiles: [
     {
-      value: 49,
-      disabled: true,
-      files: [
-        'md-components-notifications-heads-up',
-        'md-components-cards-welcome-back',
-        'md-components-tabs-status-bar',
-        'md-components-cards-safari',
-        'md-components-date-picker',
-        'md-components-chips-open-chip',
-        'md-components-cards-homes',
-        'md-components-buttons-lights',
-        'md-components-cards-pooch',
-        'md-components-buttons-fabs-light'
-      ]
-    },
-    {
-      value: 50,
-      disabled: true,
-      files: [
-        'md-components-notifications-heads-up',
-        'md-components-cards-welcome-back',
-        'md-components-tabs-status-bar',
-        'md-components-cards-safari',
-        'md-components-date-picker',
-        'md-components-chips-open-chip',
-        'md-components-cards-homes',
-        'md-components-buttons-lights',
-        'md-components-cards-pooch',
-        'md-components-buttons-fabs-light'
-      ]
-    },
-    {
       value: 51,
       disabled: false,
       files: [
@@ -70,6 +38,22 @@ export const environment = {
     },
     {
       value: 53,
+      disabled: false,
+      files: [
+        'md-components-notifications-heads-up',
+        'md-components-cards-welcome-back',
+        'md-components-tabs-status-bar',
+        'md-components-cards-safari',
+        'md-components-date-picker',
+        'md-components-chips-open-chip',
+        'md-components-cards-homes',
+        'md-components-buttons-lights',
+        'md-components-cards-pooch',
+        'md-components-buttons-fabs-light'
+      ]
+    },
+    {
+      value: 59,
       disabled: false,
       files: [
         'md-components-notifications-heads-up',
