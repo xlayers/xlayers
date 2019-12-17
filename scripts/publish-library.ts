7198d26148170ffc6e3868ea451990eccae1fe71
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2019-12-17 17:43:38 +0100

    chore: add publish script

---
+++ scripts/publish-library.ts
@@ -0,0 +1,16 @@
+import { cmd, runTask, packages } from './utils';
+
+export async function buildAllLibrarys() {
+    const pkg = process.argv[2];
+    if (!pkg) {
+        throw new Error('There is no package specified');
+    } else if (!packages.includes(pkg)) {
+        throw new Error('There is no package from our list');
+
+    }
+    await process.chdir(`./projects/${pkg}`);
+    await cmd('npx', [`release-it --ci`]);
+}
+
+runTask('Publish library', async () => await buildAllLibrarys());
+
