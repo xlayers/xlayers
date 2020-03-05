5f9ae2434f93cc02e76b4a65f886788e669c0309
Wassim Chegham <github@wassim.dev>
2020-03-05 11:11:09 +0100

    style: fix formating

--- projects/angular-codegen/src/lib/angular-codegen.service.ts
+++ projects/angular-codegen/src/lib/angular-codegen.service.ts
@@ -158,8 +158,8 @@ export class XlayersModule {}`;
 
   private renderImports(files) {
     return [
-      "import { NgModule } from '@angular/core';",
-      "import { CommonModule } from '@angular/common';"
+      'import { NgModule } from \'@angular/core\';',
+      'import { CommonModule } from \'@angular/common\';'
     ]
       .concat(
         files
