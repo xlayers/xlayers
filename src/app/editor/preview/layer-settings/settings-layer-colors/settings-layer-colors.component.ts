3a259f41af6291b9bb43344570e439de1a465474
Wassim Chegham <github@wassim.dev>
2020-09-15 14:54:31 +0200

    chore: wip

--- src/app/editor/preview/layer-settings/settings-layer-colors/settings-layer-colors.component.ts
+++ src/app/editor/preview/layer-settings/settings-layer-colors/settings-layer-colors.component.ts
@@ -1,37 +1,52 @@
 import { Store } from '@ngxs/store';
 import { Component, OnInit } from '@angular/core';
-import { UiState, LayerCSS } from '@app/core/state';
+import { UiState } from '@app/core/state';
 
 @Component({
   selector: 'xly-settings-layer-colors',
   template: `
-  <mat-expansion-panel expanded="false" [disabled]="!bgColor || gradients.length === 0">
-    <mat-expansion-panel-header>
-      <mat-panel-title>
-      {{'SETTINGS_COLORS.title' | translate}}
-      </mat-panel-title>
-    </mat-expansion-panel-header>
+    <mat-expansion-panel
+      expanded="false"
+      [disabled]="!bgColor || gradients.length === 0"
+    >
+      <mat-expansion-panel-header>
+        <mat-panel-title>
+          {{ 'SETTINGS_COLORS.title' | translate }}
+        </mat-panel-title>
+      </mat-expansion-panel-header>
 
+      <mat-nav-list>
+        <mat-list-item
+          [matMenuTriggerFor]="menuColor"
+          *ngFor="let g of gradients"
+        >
+          <span
+            class="gradient-color"
+            [style.backgroundColor]="g.backgroundColor"
+          ></span>
 
-    <mat-nav-list>
-      <mat-list-item [matMenuTriggerFor]="menuColor" *ngFor="let g of gradients">
-        <span class="gradient-color" [style.backgroundColor]="g.backgroundColor"></span>
+          <mat-menu
+            #menuColor="matMenu"
+            class="menu-color-panel"
+            [overlapTrigger]="false"
+          >
+            <color-sketch [color]="g.backgroundColor"></color-sketch>
+          </mat-menu>
+        </mat-list-item>
+        <mat-list-item [matMenuTriggerFor]="menuColor" *ngIf="bgColor">
+          <span class="gradient-color" [style.backgroundColor]="bgColor"></span>
+          {{ bgColor }}
 
-        <mat-menu #menuColor="matMenu" class="menu-color-panel" [overlapTrigger]="false">
-          <color-sketch [color]="g.backgroundColor"></color-sketch>
-        </mat-menu>
-
-      </mat-list-item>
-      <mat-list-item [matMenuTriggerFor]="menuColor" *ngIf="bgColor">
-        <span class="gradient-color" [style.backgroundColor]="bgColor"></span> {{bgColor}}
-
-        <mat-menu #menuColor="matMenu" class="menu-color-panel" [overlapTrigger]="false">
-          <color-sketch [color]="bgColor"></color-sketch>
-        </mat-menu>
-      </mat-list-item>
-    </mat-nav-list>
-
-  </mat-expansion-panel>
+          <mat-menu
+            #menuColor="matMenu"
+            class="menu-color-panel"
+            [overlapTrigger]="false"
+          >
+            <color-sketch [color]="bgColor"></color-sketch>
+          </mat-menu>
+        </mat-list-item>
+      </mat-nav-list>
+    </mat-expansion-panel>
   `,
   styles: [
     `
@@ -54,18 +69,18 @@ import { UiState, LayerCSS } from '@app/core/state';
   ]
 })
 export class SettingsLayerColorsComponent implements OnInit {
-  componentStyle: LayerCSS;
+  componentStyle: XLayersCSSContext;
   gradients: Array<{
     backgroundColor: string;
     stop: string;
   }>;
   bgColor: string;
-  constructor(private readonly store: Store) { }
+  constructor(private readonly store: Store) {}
 
   ngOnInit() {
     this.store.select(UiState.currentLayer).subscribe(currentLayer => {
       if (currentLayer) {
-        this.componentStyle = (currentLayer as any).css as LayerCSS;
+        this.componentStyle = currentLayer.css;
         this.computeBackgourndGradient();
       } else {
         this.componentStyle = null;
@@ -82,13 +97,15 @@ export class SettingsLayerColorsComponent implements OnInit {
   computeBackgourndGradient() {
     if (this.componentStyle) {
       if (this.isBackgroundGradient()) {
-        this.gradients = this.componentStyle['background-color'].match(/(rgba[ %\(\),0-9]+)/gu).map(match => {
-          const m = match.split(' ');
-          return {
-            backgroundColor: m[0],
-            stop: m[1]
-          };
-        });
+        this.gradients = this.componentStyle['background-color']
+          .match(/(rgba[ %\(\),0-9]+)/gu)
+          .map(match => {
+            const m = match.split(' ');
+            return {
+              backgroundColor: m[0],
+              stop: m[1]
+            };
+          });
       } else {
         this.bgColor = this.componentStyle['background-color'];
       }
