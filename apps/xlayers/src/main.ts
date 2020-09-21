aa6e7e7b61bb71c9366cad111351a4c50d582e03
Wassim Chegham <github@wassim.dev>
2020-09-21 15:30:49 +0200

    chore: fix post nx migration config

--- apps/xlayers/src/main.ts
+++ apps/xlayers/src/main.ts
@@ -14,21 +14,20 @@ if (environment.production) {
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
-// if (environment.hmr) {
-// if (module['hot']) {
-// hmrBootstrap(module, bootstrap);
-// } else {
-console.error('HMR is not enabled for webpack-dev-server!');
-console.log('Are you using the --hmr flag for ng serve?');
-// }
-// } else {
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch((err) => {
+    gtag('event', 'exception', {
+      description: err,
+      fatal: false,
+    });
 
-bootstrap().catch((err) => {
-  gtag('event', 'exception', {
-    description: err,
-    fatal: false,
+    console.log(err);
   });
-
-  console.log(err);
-});
-// }
+}
