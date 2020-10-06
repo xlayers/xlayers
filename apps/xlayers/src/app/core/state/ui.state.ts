97c3c6190240eebcb6175f98f1b8378109d9a8f6
Jefiozie <jefiozie.bosch@gmail.com>
2020-10-06 13:26:11 +0200

    fix: typo in the variable

--- apps/xlayers/src/app/core/state/ui.state.ts
+++ apps/xlayers/src/app/core/state/ui.state.ts
@@ -329,14 +329,14 @@ export class UiState {
 
   @Action(InformUser)
   informUser({}, action: InformUser) {
-    let errorNessage = action.message;
+    let errorMessage = action.message;
     if ((action.message as any) instanceof Event) {
-      errorNessage = (action?.message as any)?.target?.error.message;
+      errorMessage = (action?.message as any)?.target?.error.message;
     }
 
     this.snackBar
       .open(
-        errorNessage,
+        errorMessage,
         action.errorType === ErrorType.None ? 'CLOSE' : 'REPORT',
         {
           duration: action.errorType === ErrorType.None ? 5000 : 0,
@@ -345,7 +345,7 @@ export class UiState {
       .onAction()
       .subscribe(() => {
         if (action.errorType !== ErrorType.None) {
-          const githubIssueUrl = `template=bug_report.md&title=${errorNessage}`;
+          const githubIssueUrl = `template=bug_report.md&title=${errorMessage}`;
           window.open(
             `https://github.com/xlayers/xlayers/issues/new?${githubIssueUrl}`,
             '__blank'
