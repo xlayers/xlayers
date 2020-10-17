21a53cc570df45d70916e0f0c84fbf938b1903f1
Ane DIAZ DE TUESTA <adiazdetuesta@idmog.com>
2020-10-17 12:03:59 +0200

    feat: create a new file with a method to get files' information

---
+++ apps/xlayers/src/app/supported-files.ts
@@ -0,0 +1,24 @@
+/**
+ * Object that contains information about file types xlayers is compatible with
+ */
+const compatibleFiles = [
+  {
+    name: 'Sketch App',
+    extension: '.sketch',
+    logo: '/assets/supported/sketch.svg',
+    supportedVersions: [49, 50, 51, 52, 53, 59],
+  },
+];
+
+/**
+ * It returns information related to file, based on it's extension
+ * @param fileName the file's complete name (extension included)
+ */
+export function getFileData(fileName: string) {
+  const fileExtension = `.${fileName.split('.').pop()}`; // get file's extension with a point at the begining
+  const name = fileName.replace(fileExtension, ''); // extract file's name without the extension
+  const compatible = compatibleFiles.find(
+    ({ extension }) => fileExtension === extension
+  ) || { logo: '' }; // get information related to the file based on the extension
+  return { name, logo: compatible.logo, extension: fileExtension }; //for the moment, minimum data is returned
+}
