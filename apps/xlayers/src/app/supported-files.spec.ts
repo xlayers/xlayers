8f6b5dcdf013755542780d03a328eba3e3add0cd
Ane Diaz de Tuesta <anediaz@gmail.com>
2020-11-14 17:45:40 +0100

    [FIX] Fix forgotten failing tests

--- apps/xlayers/src/app/supported-files.spec.ts
+++ apps/xlayers/src/app/supported-files.spec.ts
@@ -6,21 +6,24 @@ describe('supported-files', () => {
       const expected = {
         name: 'my-wonderful-design',
         icon: '/assets/supported/sketch.svg',
+        version: '5.1',
       };
-      expect(toTest.getFileData('my-wonderful-design.sketch')).toEqual(
+      expect(toTest.getFileData('my-wonderful-design.sketch', '5.1')).toEqual(
         expected
       );
     });
     it('should return name and extension, without icon, if file type not supported', () => {
-      expect(toTest.getFileData('slides.ppt')).toEqual({
+      expect(toTest.getFileData('slides.ppt', '')).toEqual({
         name: 'slides',
         icon: '',
+        version: '',
       });
     });
     it('should return only the name if extension is not precised on the fileName', () => {
-      expect(toTest.getFileData('notes')).toEqual({
+      expect(toTest.getFileData('notes', '2.0')).toEqual({
         name: 'notes',
         icon: '',
+        version: '2.0',
       });
     });
   });
