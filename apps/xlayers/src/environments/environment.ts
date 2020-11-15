df2c03011d0edd429958cf671d10513aa32705b0
Ane Diaz de Tuesta <anediaz@gmail.com>
2020-11-15 21:32:00 +0100

    chore: add v69 files to demo files folder (#393)
    * feat: Add v69 files to demo files folder
    
    * feat: add v69 demo files to environment files
    
    * Update apps/xlayers/src/environments/environment.hmr.ts
    
    * Update apps/xlayers/src/environments/environment.prod.ts
    
    * Update apps/xlayers/src/environments/environment.ts
    
    Co-authored-by: Wassim Chegham <1699357+manekinekko@users.noreply.github.com>

--- apps/xlayers/src/environments/environment.ts
+++ apps/xlayers/src/environments/environment.ts
@@ -72,6 +72,22 @@ export const environment = {
         'md-components-buttons-fabs-light',
       ],
     },
+    {
+      value: 69,
+      disabled: false,
+      files: [
+        'md-components-notifications-heads-up',
+        'md-components-cards-welcome-back',
+        'md-components-tabs-status-bar',
+        'md-components-cards-safari',
+        'md-components-date-picker',
+        'md-components-chips-open-chip',
+        'md-components-cards-homes',
+        'md-components-buttons-lights',
+        'md-components-cards-pooch',
+        'md-components-buttons-fabs-light',
+      ],
+    },
   ],
 };
 
