9e4c34c7a6deb9ff7a12ae10a94b7a8c7328d930
Remi <remi.d45@gmail.com>
2020-11-20 00:33:51 +0100

    feat(editor): update framework selection

--- apps/xlayers/src/app/editor/code/editor-container/editor-container.component.ts
+++ apps/xlayers/src/app/editor/code/editor-container/editor-container.component.ts
@@ -1,83 +1,95 @@
-import {
-  AfterContentInit,
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { CodeGenKind, CodeGenService } from './codegen/codegen.service';
 import { PAGE_DOWN, PAGE_UP } from '@angular/cdk/keycodes';
-import { CodeGenSettings, CodeGen } from '../../../core/state/page.state';
+import {
+  CodeGenSettings,
+  CodeGen,
+  CodeGenState,
+} from '../../../core/state/page.state';
 
 const githubIssueLink =
   // tslint:disable-next-line:max-line-length
   'https://github.com/xlayers/xlayers/issues/new?assignees=&labels=Priority%3A+Low%2C+Scope%3A+CodeGen%2C+community-help%2C+effort2%3A+medium+%28days%29%2C+good+first+issue%2C+type%3A+discussion+%2F+RFC&template=codegen--add---technology---support.md&title=CodeGen%3A+add+%5B%5Btechnology%5D%5D+support';
+
+interface Framework {
+  label: string;
+  svgIcon: string;
+  codegen: CodeGenKind;
+}
 
 @Component({
   selector: 'xly-editor-container',
   templateUrl: './editor-container.component.html',
   styleUrls: ['./editor-container.component.css'],
 })
-export class EditorContainerComponent implements OnInit, AfterContentInit {
+export class EditorContainerComponent implements OnInit {
   codeSetting: CodeGenSettings;
   @ViewChild('codeContentEditor') codeEditor: ElementRef;
 
-  frameworks: Array<{
-    title: string;
-    logo: FunctionStringCallback;
-  }>;
+  public frameworks: Framework[] = [
+    {
+      label: 'Angular',
+      svgIcon: 'angular',
+      codegen: CodeGenKind.Angular,
+    },
+    {
+      label: 'Angular Element',
+      svgIcon: 'angularElement',
+      codegen: CodeGenKind.AngularElement,
+    },
+    {
+      label: 'Vue',
+      svgIcon: 'vue',
+      codegen: CodeGenKind.Vue,
+    },
+    {
+      label: 'React',
+      svgIcon: 'react',
+      codegen: CodeGenKind.React,
+    },
+    {
+      label: 'WebComponents',
+      svgIcon: 'wc',
+      codegen: CodeGenKind.WC,
+    },
+    {
+      label: 'Stencil',
+      svgIcon: 'stencil',
+      codegen: CodeGenKind.Stencil,
+    },
+    {
+      label: 'LitElement',
+      svgIcon: 'litElement',
+      codegen: CodeGenKind.LitElement,
+    },
+  ];
+
+  public selectedFramework: Framework = this.frameworks[0];
 
   constructor(private codegen: CodeGenService, private readonly store: Store) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.store.select(CodeGenState.codegen).subscribe((codegen) => {
+      if (codegen.kind) {
+        this.selectedFramework = this.frameworks.find(
+          (framework) => framework.codegen === codegen.kind
+        );
+      }
+      this.codeSetting = this.codegen.generate(codegen.kind);
+    });
+  }
 
-  ngAfterContentInit() {
-    this.generateAngular();
+  onChange(event: any) {
+    const {
+      value: { codegen },
+    } = event;
+    this.codeSetting = this.codegen.generate(codegen);
+    this.updateState();
   }
 
   onEditorInit(editor: any, ctrl: HTMLElement) {
     ctrl.focus();
-  }
-
-  generateAngular() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.Angular);
-    this.updateState();
-  }
-
-  generateAngularElement() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.AngularElement);
-    this.updateState();
-  }
-
-  generateReact() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.React);
-    this.updateState();
-  }
-
-  generateVue() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.Vue);
-    this.updateState();
-  }
-
-  generateWc() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.WC);
-    this.updateState();
-  }
-
-  generateStencil() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.Stencil);
-    this.updateState();
-  }
-
-  generateLitElement() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.LitElement);
-    this.updateState();
-  }
-
-  generateXamarinForms() {
-    this.codeSetting = this.codegen.generate(CodeGenKind.XamarinForms);
-    this.updateState();
   }
 
   updateState() {
