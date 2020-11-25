91f3e3d485b2f0d9669fab07aab06a3e9a7fa605
Remi <remi.d45@gmail.com>
2020-11-25 18:01:33 +0100

    refactor(core): made the codegen list shareable

---
+++ apps/xlayers/src/shared/codegen-list.ts
@@ -0,0 +1,45 @@
+import { CodeGenKind } from '@xlayers-apps/app/editor/code/editor-container/codegen/codegen.service';
+
+export interface UICodeGen {
+  label: string;
+  svgIcon: string;
+  codegenType: CodeGenKind;
+}
+
+export const codeGenList: UICodeGen[] = [
+  {
+    label: 'Angular',
+    svgIcon: 'angular',
+    codegenType: CodeGenKind.Angular,
+  },
+  {
+    label: 'Angular Element',
+    svgIcon: 'angularElement',
+    codegenType: CodeGenKind.AngularElement,
+  },
+  {
+    label: 'Vue',
+    svgIcon: 'vue',
+    codegenType: CodeGenKind.Vue,
+  },
+  {
+    label: 'React',
+    svgIcon: 'react',
+    codegenType: CodeGenKind.React,
+  },
+  {
+    label: 'WebComponents',
+    svgIcon: 'wc',
+    codegenType: CodeGenKind.WC,
+  },
+  {
+    label: 'Stencil',
+    svgIcon: 'stencil',
+    codegenType: CodeGenKind.Stencil,
+  },
+  {
+    label: 'LitElement',
+    svgIcon: 'litElement',
+    codegenType: CodeGenKind.LitElement,
+  },
+];
