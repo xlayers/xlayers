0bfbf91d813de389364606c2e2814d1f02c1be25
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2020-12-20 19:34:00 +0100

    V11 (#402)
    * chore: update v11 dependensies
    
    * chore: update nrwl nx to v11
    
    * chore: update secondary packages to v11
    
    * chore: update to latest nx version
    
    * chore: update to nx 11.0.8
    
    * chore: update to latest nx app
    
    * chore: update to latest NX
    
    * test: resolve e2e tests

--- libs/sketchtypes/jest.config.js
+++ libs/sketchtypes/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'sketchtypes',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/sketchtypes',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'sketchtypes',
 };
