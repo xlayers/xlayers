d288d1c639dd6b3b64e8e7eec3296917fc42de9a
Jefiozie <jefiozie.bosch@gmail.com>
2021-07-19 20:03:24 +0200

    chore: update all packages to lastest nx 12.1.0

--- libs/sketchtypes/jest.config.js
+++ libs/sketchtypes/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/sketchtypes',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
   displayName: 'sketchtypes',
+  testEnvironment: 'node',
 };
